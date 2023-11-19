2979439d82c3c31bc271c46718cb3dc33bc1467f
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2023-11-19 20:02:10 +0800

    Add files via upload

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "stephen"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "stephenCoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "STPC"
 )
 
 var ID ids.ID
